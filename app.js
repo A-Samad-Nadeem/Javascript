e19c4084489ace235ac5f5b42d1524d5f88f7918
A-Samad-Nadeem <154529306+A-Samad-Nadeem@users.noreply.github.com>
2024-02-29 11:31:28 +0500

    Add files via upload
    my first javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Error! Please Enter A Valid Password");
+alert("Welcome To JS Land...\nHappy Coding!");
+alert("Welcome To JS Land")
+alert("Happy Coding!")
+alert("Hello...I Can Run JS Through MY Web Browser Console")
+
+console.log(alert)
