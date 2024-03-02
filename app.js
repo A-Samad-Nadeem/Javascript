bc49d6386c058676423bbdaf1e4bedd9cf778364
A-Samad-Nadeem <154529306+A-Samad-Nadeem@users.noreply.github.com>
2024-03-02 20:45:27 +0500

    Add files via upload
    second project

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-alert("Error! Please Enter A Valid Password");
-alert("Welcome To JS Land...\nHappy Coding!");
-alert("Welcome To JS Land")
-alert("Happy Coding!")
-alert("Hello...I Can Run JS Through MY Web Browser Console")
+var myname ="abdulsamad";
+alert(myname);
+var myage = 18; 
+alert(myage);
+var number = 654; 
+alert(number);
+var mycourse ="Web and Mobile And App Development";
+alert(mycourse);
 
-console.log(alert)
+var pizza ="pizza";
+alert(pizza);
+var pizz ="pizz";
+alert(pizz);
+var piz ="piz";
+alert(piz);
+var pi ="pi";
+alert(pi);
+var p ="p";
+alert(p);
+var email ="myemailissamadnadeem555"
+alert(email);
+var book =" I  am learning JS from A smarter way to learn JavaScript"
+alert(book);
+
+var display ="Yah! I can write HTML through JS"
+document.write(display);
+var symbol ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(symbol);
