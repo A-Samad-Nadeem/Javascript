8866da46438c3b178091a775bccc3c3c5f4e3924
A-Samad-Nadeem <154529306+A-Samad-Nadeem@users.noreply.github.com>
2024-03-07 13:40:18 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-let number1 =+prompt("Enter First Number")
-let number2 =+prompt("Enter Second Number")
+// question no 1
 
-let sum=number1+number2;
-document.write("addition of number=" ,sum,"<br/>")
-let sub=number1-number2;
-document.write("Subtraction of number= ",sub,"<br/>")
-let multiplication=number1*number2;
-document.write("Product of number= ",multiplication,"<br/>")
-let div=number1/number2;
-document.write("division of number= ",div,"<br/>")
-let reminder=number1%number2;
-document.write("reminder of number= ",reminder,"<br/>")
+// let variable=("samad");
+// let oneVariable=("hello world");
+// let variable_two=("salam");
+
+// document.write(variable,oneVariable,variable_two);
+
+// // Question no2
+// // 5 Legal variable
+// // =================
+
+// let legal=("hello");
+// let legal1=("world");
+// let $=("hi");
+// let _=("greetings");
+// let Saylani_it_Training=("Sir Sadiq");
+
+// // 5 Illegal Variables
+// // ===================
+
+
+// let 123=("hi");
+
+// let *=("salam");
+
+// let sam of he=("Salute");
+
+// let userAlert=("greetings");
+
+// let -samad=("samad");
+ 
+// Question no 3
+document.write("<h1>Rule Of Naming JS Variables</h1>");
+
+
+document.write("<P>variable names can only contain, alphabets,$,_ </P>")
+document.write("<P>variable names must begin with $,_ or a letter</P>")
+document.write("<P>variable names are case sensitive</P>")
+document.write("<P>variable names can not be JS reserved keywords</P>")
+
 
