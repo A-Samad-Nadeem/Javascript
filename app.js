711b09b99b50d4a68fdf455617a41b535c6c8803
A-Samad-Nadeem <154529306+A-Samad-Nadeem@users.noreply.github.com>
2024-03-26 13:52:29 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,212 @@
-let count=0;
+// let city=prompt("please enter yor city name")
 
-document.getElementById("increament").onclick=function(){
-    count+=1
-    document.getElementById("ginti") .innerHTML=count;
-}
-document.getElementById("reset").onclick=function(){
-    count=0
-    document.getElementById("ginti").innerHTML=count;
-}
+// if (city === "karachi") {alert("Welcome To The City OF Lights")
+
+// }else{
+//     alert("Have A Good Day")
+// }
+
+
+// let gender= prompt("PLease Define Your Gender")
+
+// if(gender === "male") {
+//     document.write("Have A Good DAy Sir","</br>")
+// }else{
+//     document.write("Have a Good Day Madam","</br>")
+// }
+
+
+// let trafficlights=prompt("Which Color Light Are you Watching ")
+             
+// if(trafficlights === "red"){
+//     document.write("Stop")
+// }
+// if(trafficlights === "yellow"){
+//     document.write("Start Your Engines")
+// }
+// if(trafficlights === "green"){
+//     document.write("Pehli Fursat me nikal")
+// }else{
+//     document.write("kia Dubai Me he??")
+// }
+
+// let fuel=+prompt("How much litre petrol is in your car now ")
+
+// if(fuel < 0.25){
+
+//     alert("please refill your tank")
+// }
+// else{
+//     alert("have a good day")
+// }
+
+// let usersubject1=+prompt("Enter Your First Subject Number")
+// let usersubject2=+prompt("Enter Your second Subject Number")
+// let usersubject3=+prompt("Enter Your third Subject Number")
+// let usersubject4=+prompt("Enter Your Fourth Subject Number")
+
+// let total=100
+
+// let userper1=(usersubject1/total)*100
+// let userper2=(usersubject2/total)*100
+// let userper3=(usersubject3/total)*100
+// let userper4=(usersubject4/total)*100
+
+
+// let excel="Excellent"
+// let good="Good"
+// let imprve="Needimprovement"
+// let fail="fail"
+
+// if(userper1>=80){
+//     document.getElementById("remark1").innerText=excel
+// }
+// else{
+//     document.getElementById("remark1").innerText=good
+// }
+// if(userper1>=70){
+//     document.getElementById("remark2").innerText=good
+// }
+// else{
+//     document.getElementById("remark2").innerText=imprve
+// }
+// if(userper1>=60){
+//     document.getElementById("remark3").innerText=imprve
+// }
+// else{
+//     document.getElementById("remark3").innerText=fail
+// }
+// if(userper1>=60){
+//     document.getElementById("remark4").innerText=fail
+// }
+// else{
+//     document.getElementById("remark4").innerText=fail
+// }
+// let grade1="A-One"
+// let grade2="B"
+// let grad3="C"
+// let grad4="fail"
+
+// if(userper1>=80){
+//     document.getElementById("grade1").innerText=grade1
+// }
+// else{
+//     document.getElementById("grade1").innerText=grade2
+// }
+// if(userper1>=70){
+//     document.getElementById("grade2").innerText=grade2
+// }
+// else{
+//     document.getElementById("grade2").innerText=grad3
+// }
+// if(userper1>=60){
+//     document.getElementById("grade3").innerText=grad3
+// }
+// else{
+//     document.getElementById("grade3").innerText=grad4
+// }
+// if(userper1>=60){
+//     document.getElementById("grade4").innerText=grad4
+// }
+// else{
+//     document.getElementById("grade4").innerText=grad4
+// }
+
+
+// document.write("<h1>MarkSheet</h1>")
+// let obtained=usersubject1+usersubject2+usersubject3+usersubject4
+
+// document.write("<h2>Obtained Marks=</h2>",obtained,"</br>")
+
+// let tpercent=(obtained/400)*100
+
+// document.write("<h2>TOtal Percentage=</h2>",tpercent,"</br>")
+
+// let finalg1="A-One"
+// let finalg2="B"
+// let finalg3="C"
+// let finalg4="Propmoted"
+
+// if(tpercent>=80){
+//     document.write("<h2>Grade=</h2>",finalg1,"</br>","<h2>Remarks=</h2>", "<h2>Excellent</h2>")
+// }
+// else{
+//     document.write("<h2>Grade=</h2>",finalg2,"</br>","<h2>Remarks=</h2>", "<h2>good</h2>")
+// }
+// if(tpercent>=70){
+//     document.write("<h2>Grade=</h2>",finalg2,"</br>","<h2>Remarks=</h2>", "<h2>Good</h2>")
+// }
+// else{
+//     document.write("<h2>Grade=</h2>",finalg3,"</br>","<h2>Remarks=</h2>", "<h2>Need Improvement</h2>")
+// }
+// if(tpercent>=60){
+//     document.write("<h2>Grade=</h2>",finalg3,"</br>","<h2>Remarks=</h2>", "<h2>Need Improvement</h2>")
+// }
+// else{
+//     document.write("<h2>Grade=</h2>",finalg4,"</br>","<h2>Remarks=</h2>", "<h2>Fail</h2>")
+// }
+// if(tpercent<=60){
+//     document.write("<h2>Grade=</h2>",finalg4,"</br>","<h2>Remarks=</h2>", "<h2>VERY VERY SAD</h2>")
+// }
+// else{
+//     document.write("<h2>Grade=</h2>",finalg4,"</br>","<h2>Remarks=</h2>", "<h2>VERY VERY SAD</h2>")
+// }
+
+
+
+// let secret=7
+// let guess=+prompt("Guess The Secret Number")
+
+// if(guess===secret){
+//     alert("BINGO")
+// }
+// else{
+//     alert("TRY AGAIN")
+// }
+
+// let number=+prompt("Enter A Number")
+// if(number%3===0){
+//     alert("YES IT IS DIVISIBLE BY 3")
+// }
+// else{
+//     alert("SORRY")
+// }
+
+// let number=+prompt("Enter A Number")
+// if(number%2===0){
+//     alert("YES IT IS AN EVEN NUMBER")
+// }
+// else{
+//     alert("NO IT IS NOT A EVEN NUMBER")
+// }
+// if(number%3===0){
+//     alert("IT IS A ODD NUMBER")
+// }
+// else{
+//     alert("NO IT IS NOT A ODD NUMBER")
+// }
+
+// let temp=+prompt("ENTER THE TEMPRATURE")
+// if(temp>40){
+//     alert("IT IS TOO HOT")
+// }else{
+// alert("have a good day")
+// }
+
+// if(temp>30){
+//     alert("THE WEATHER IS NORMAL TODAY")
+// }else{
+//     alert("have a good day")
+// }
+
+// if(temp>20){
+//     alert("IT IS TOO COOL TODAY")
+// }else{
+//     alert("have a good day")
+// }
+
+// if(temp>10){
+//     alert("OMG!IT IS TOO COLD")
+// }else{
+//     alert("have a good day")
+// }
